6d15c6002b163784bd0bc624cb6fa1f4b0e5d993
dl-nice <dnice_bo@outlook.com>
2022-10-20 09:54:04 +0800

    init utils

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gotechbook-framework-utils
+
+go 1.19
---
+++ util_file.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+func WriteFile(t *testing.T, filepath string, bytes []byte) {
+	t.Helper()
+	if err := ioutil.WriteFile(filepath, bytes, 0644); err != nil {
+		t.Fatalf("failed writing file: %s", err)
+	}
+}
+
+func ReadFile(t *testing.T, filepath string) []byte {
+	t.Helper()
+	b, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		t.Fatalf("failed reading file: %s", err)
+	}
+	return b
+}
